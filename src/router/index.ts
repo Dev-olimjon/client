6dfe72bd7a8179a38f8ab6ad43144044a5d13f7b
DEV-olimjon <olimjonmakhmudov26156@gmail.com>
2022-06-20 15:04:45 +0500

    all

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,10 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import loginList from ""
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: 
   },
   {
     path: '/about',
